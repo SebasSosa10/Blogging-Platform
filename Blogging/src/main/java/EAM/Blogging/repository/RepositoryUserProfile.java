d46c22db4cb1afdbab64ac712605e394f2b91d15
Jdraxs <juandiegorojasabarkaguzman@gmail.com>
2024-10-12 00:17:12 -0500

    feat(Added Login Code)

--- Blogging/src/main/java/EAM/Blogging/repository/RepositoryUserProfile.java
+++
@@ -1,9 +0,0 @@
-package EAM.Blogging.repository;
-
-import EAM.Blogging.model.UserProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositoryUserProfile extends JpaRepository<UserProfile, Long> {
-}
